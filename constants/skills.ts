be9b98569aea194dbbf757327b82e352e05be1b9
Amit-Shaw7 <maahiamit777@gmail.com>
2023-10-30 21:50:58 +0530

    skills section added

---
+++ constants/skills.ts
@@ -0,0 +1,72 @@
+export const skillArr = [
+    {
+        id : 1,
+        name : "HTML",
+        img : "html.svg"
+    },
+    {
+        id : 2,
+        name : "CSS",
+        img : "css.svg"
+    },
+    {
+        id : 3,
+        name : "JAVASCRIPT",
+        img : "js.svg"
+    },
+    {
+        id : 4,
+        name : "BOOTSTRAP",
+        img : "bootstrap.svg"
+    },
+    {
+        id : 5,
+        name : "TAILWIND CSS",
+        img : "tailwind.svg"
+    },
+    {
+        id : 15,
+        name : "Material UI",
+        img : "mui.svg"
+    },
+    {
+        id : 7,
+        name : "GITHUB",
+        img : "github.svg"
+    },
+    {
+        id : 8,
+        name : "REACT",
+        img : "react.svg"
+    },
+    {
+        id : 9,
+        name : "NODE",
+        img : "nodejs.svg"
+    },
+    {
+        id : 10,
+        name : "EXPRESS",
+        img : "expressjs.svg"
+    },
+    {
+        id : 11,
+        name : "MONGO DB",
+        img : "mongodb.svg"
+    },
+    {
+        id : 12,
+        name : "SASS",
+        img : "scss.svg"
+    },
+    {
+        id : 13,
+        name : "Redux Toolkit",
+        img : "redux.svg"
+    },
+    {
+        id : 15,
+        name : "Next Js",
+        img : "next.svg"
+    },
+]
--- sections/Skills.tsx
+++ sections/Skills.tsx
@@ -1,9 +1,36 @@
+import Heading from '@/components/Heading'
+import { skillArr } from '@/constants/skills'
+import Image from 'next/image'
 import React from 'react'
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
+    return (
+        <div className='py-20'>
+            <Heading title='Skills' index={2} />
+            <div className='w-full flex justify-center py-20'>
+                <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 px-4 gap-10'>
+                    {
+                        skillArr.map((skill) => (
+                            <div key={skill.id} className='p-5 border border-highlight rounded-full w-max h-max relative'>
+
+                                <Image
+                                    src={`/assets/skills/${skill?.img}`}
+                                    height={50}
+                                    width={50}
+                                    objectFit='cover'
+                                    alt='Javascript'
+                                    className='rounded-md'
+                                />
+
+                                {/* <span className='-top-12 uppercase  bg-bg border border-highlight p-1 rounded-md absolute text-unselected peer-hover:flex'>Javascript</span> */}
+                                <div className='absolute inset-0 h-full w-full bg-highlight-200 opacity-40 rounded-full transition duration-300 hover:opacity-0'></div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Skills
